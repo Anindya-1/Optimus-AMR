7dc26b2ffbb7eaa39e9553b649884dac22789bf2
Anindya <eea232158@iitd.ac.in>
2024-10-28 16:15:44 +0530

    new file:   diffdrive_bot/bringup/launch/custom_cartographer.launch.py

---
+++ diffdrive_bot/bringup/launch/custom_cartographer.launch.py
@@ -0,0 +1,51 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, ExecuteProcess,\
+                           IncludeLaunchDescription, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+import xacro
+
+
+def generate_launch_description():
+    
+    pkg_name = 'diffdrive_bot'
+
+    rviz_config = os.path.join(
+      get_package_share_directory(pkg_name),
+      'rviz',
+      'cartographer.rviz'
+    )
+
+    slam_config = os.path.join(
+      get_package_share_directory(pkg_name),
+      'config',
+      'mapper_params_online_async.yaml'
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],      
+    )
+  
+    slam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('slam_toolbox'), 'launch'),
+                                       '/online_async_launch.py']),
+        launch_arguments={'slam_params_file': slam_config}.items()
+    )
+
+    return LaunchDescription([
+        slam,
+        rviz_node        
+    ])
